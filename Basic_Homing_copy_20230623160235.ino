d90b7cae6768667cdb3b4fc64dc6ba14452e3890
Catarina <catarinarubpacheco@gmail.com>
2023-07-18 13:48:02 +0200

    initial

---
+++ Basic_Homing_copy_20230623160235.ino
@@ -0,0 +1,34 @@
+/* Include necessary libraries */
+#include <ZaberAscii.h>
+
+ZaberShield shield(ZABERSHIELD_ADDRESS_AA);
+ZaberAscii za(shield);
+ 
+void setup() {
+  /* Initialize baudrate to 115200, typical for Zaber ASCII devices */
+  shield.begin(115200);
+
+  /* Issue a command for initial position */
+  za.send(1, "move abs", 1);
+  /* Always read the reply even if you don't check it for errors. */
+  za.receive();
+  /* Wait for the move command to finish moving. */
+  za.pollUntilIdle(1);
+
+}
+ 
+void loop() {
+ /* Issue a position */
+ za.send(1, "move abs", 533333);
+ za.receive();
+ /* Wait for the move command to finish moving. */
+ za.pollUntilIdle(1);
+
+ delay (500);
+
+ /* Issue a position */
+ za.send(1, "move abs", 1);
+ za.receive();
+ za.pollUntilIdle(1);
+}
+
