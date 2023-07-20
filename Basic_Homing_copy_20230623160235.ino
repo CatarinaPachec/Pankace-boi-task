89c46f2514834c2f40a9b5f9c1f78b76c2dae4fc
Catarina <catarinarubpacheco@gmail.com>
2023-07-20 11:04:01 +0200

    Input & output

--- Basic_Homing_copy_20230623160235.ino
+++ Basic_Homing_copy_20230623160235.ino
@@ -4,43 +4,53 @@
 ZaberShield shield(ZABERSHIELD_ADDRESS_AA);
 ZaberAscii za(shield);
 
-boolean ShouldMove = true;
+const int pin = 2;
+const int LEDpin = 4;
+int lastState = LOW;
+
  
 void setup() {
   Serial.begin(9600);
+  shield.begin(115200); /* Initialize baudrate to 115200, typical for Zaber ASCII devices */
 
-  /* Initialize baudrate to 115200, typical for Zaber ASCII devices */
-  shield.begin(115200);
+  MoveBackwards();
 
-  /* Issue a command for initial position */
-  za.send(1, "move abs", 1);
-  /* Always read the reply even if you don't check it for errors. */
-  za.receive();
-  /* Wait for the move command to finish moving. */
-  za.pollUntilIdle(1);
+  pinMode(pin, INPUT);
+  pinMode(LEDpin, OUTPUT);
 }
  
 void loop() {
- /* Issue a position */
-  if (ShouldMove==true) {
-    Serial.println("Movement Start");
 
+int currentState = digitalRead(pin);
+
+if (lastState == LOW & currentState == HIGH) {
+   MoveForward();
+
+   digitalWrite(LEDpin, HIGH);
+   delay(500);
+   digitalWrite(LEDpin, LOW);
+
+   MoveBackwards();
+   
+   lastState = HIGH;
+   }
+else if (currentState == LOW){
+  lastState = LOW;
+}
+
+}
+
+void MoveForward (){
     za.send(1, "move abs", 533333);
     za.receive();
-    /* Wait for the move command to finish moving. */
     za.pollUntilIdle(1);
 
-    Serial.println("Pause");
-    delay (500);
+    Serial.println("Moving forward");
+}
 
-    /* Issue a position */
+void MoveBackwards (){
     za.send(1, "move abs", 1);
     za.receive();
     za.pollUntilIdle(1);
-
-    Serial.println("Movement End");
-
-    ShouldMove = false;
-  }
 }
 
