b8e854776174ac73329597c357bd8e8e3e5326d9
Catarina <catarinarubpacheco@gmail.com>
2023-08-02 12:14:12 +0200

    Opening signal

--- LearningProject/LearningProject.ino
+++ LearningProject/LearningProject.ino
@@ -4,23 +4,40 @@ Servo servo;
 const int servoPin = 4;
 const int Pin = 5;
 bool moveRight = true; // Flag to determine the movement direction
+bool movesignal = false;
+bool signal = LOW; 
 
 void setup() {
+  Serial.begin(9600);
   pinMode(Pin, INPUT);
 
-  servo.attach(servoPin);
-  servo.write(0);
+  // servo.attach(servoPin);
+  // servo.write(0);
 }
 
 void loop() {
-  if (moveRight) {
-    servo.write(50);
-  } else {
-    servo.write(0);
+int signal = digitalRead(Pin);
+// Serial.println(signal);
+
+if (signal == HIGH) {
+  movesignal = true;
   }
-  
-  delay(1000); // Adjust the delay time to change the speed of movement
-  
-  moveRight = !moveRight; // Toggle the flag for the next iteration
+
+if (movesignal == true) {
+    if (moveRight) {
+      //  servo.write(50);
+       Serial.println("move right");
+       delay(1000); 
+       moveRight = !moveRight;
+     } else {
+      //  servo.write(0);
+       Serial.println("move left");
+       moveRight = !moveRight;
+       movesignal = false;
+       Serial.println("move stop");
+    }
+
+  }
+
 }
 
